4fccb40970288609ad8174039f736acbf48a2335
perlancar (on pc-office) <perlancar@gmail.com>
2019-07-24 18:19:24 +0700

    Initial commit

---
+++ lib/App/LanguageExprUtils.pm
@@ -0,0 +1,26 @@
+package App::SahUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Language::Expr
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Language::Expr>
+
+=cut
